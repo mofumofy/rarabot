86509bf1ae475bcd76acb26f483aa7498ab045f2
mofumofy <keiranefre@gmail.com>
2022-12-22 00:05:30 +1300

    make it better

--- script.py
+++ script.py
@@ -38,7 +38,7 @@ def job():
     idx = random.randrange(0, len(df.index))
     tweet_now(df.iloc[idx, 0])
 
-schedule.every().day.at("00:00:00").do(job)
+schedule.every().day.at("00:06").do(job)
 
 
 while True:
